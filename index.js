aed94cb19a7da0d59c3e7015eddb4219415a0571
Алексей Рыбак <rybak2108@yandex.ru>
2024-09-30 22:49:13 +0300

    Lesson_1.18.4

--- index.js
+++ index.js
@@ -1,12 +1,38 @@
-let isVaccinated = true;
-
-const cat = {
-  age: 5,
-  weigth: 3,
-  name: "Мурзик",
-  isVaccinated: true,
-};
-
-
-
-console.log(cat);
+const childRolesEl = document.getElementById("roles-children");
+const input = document.getElementById("oneMoreChild");
+const button = document.getElementById("addChildButton");
+let numberChildren = childRolesEl.children.length;
+ 
+input.addEventListener("keypress", (event) => {
+  if (event.key === "Enter") {
+    addChild();
+  }
+});
+ 
+button.addEventListener("click", () => {
+  addChild();
+});
+ 
+function addChild() {
+  const oneMoreChildInputText = input.value.trim();
+ 
+  if (oneMoreChildInputText !== "") {
+    childRolesEl.innerHTML += `<li class="item">${oneMoreChildInputText} <button class="delete-button">Удалить</button></li>`;
+ 
+    // Навешиваем обработчик на все кнопки удаления
+    const deleteButtons = document.querySelectorAll(".delete-button");
+    deleteButtons.forEach((btn) => {
+      btn.addEventListener("click", () => {
+        btn.parentElement.remove();
+        numberChildren--;
+        button.disabled = false;
+      });
+    });
+  }
+ 
+  input.value = "";
+ 
+  numberChildren++;
+ 
+  if (numberChildren === 5) button.disabled = true;
+}
