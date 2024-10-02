f5490ac9540f3be7c18ca2b90645ef3974dca96e
Алексей Рыбак <rybak2108@yandex.ru>
2024-10-02 12:30:06 +0300

    task

--- quiz.js
+++ quiz.js
@@ -1,28 +1,35 @@
-// Получаем все элементы с классом "answer"
-const answers = document.querySelectorAll(".answer");
-let isCorrectByIndex;
-// Получаем элемент для отображения результата
-const resultElement = document.querySelector(".result");
+// 1. Получаем все элементы с классом "answer"
+const answers = document.querySelectorAll();
 
-// Добавляем обработчик события на каждый элемент с классом "answer"
+// 2. Добавляем обработчик события на каждый элемента с классом "answer"
 answers.forEach((answer, index) => {
+  // 3. Порядковый номер элемента с классом "answer" - это index
   answer.addEventListener("click", () => {
-    // Выделяем выбранный ответ
+
+    // 11. Вариант hard: снимаем выделение с других ответов
+
+    // 4. Выделяем выбранный ответ, меняя стили 
     answer.style.backgroundColor = "#007bff";
     answer.style.color = "#fff";
 
-    // Проверяем порядковый номер ответа
-    if (index === 1) {
-      isCorrectByIndex = true;
+    let isCorrectByIndex; // 7. Объявляем переменную, которая будет менять значение с false на true при правильном ответе
+
+    // 5. Проверяем порядковый номер ответа
+    if () {
+      // 6. Второй вариант ответа, у которого index === 1, правильный
+       // 8. Присваиваем значение переменной в зависимости от верности или неверности ответа
     } else {
-      isCorrectByIndex = false;
+      
     }
 
-    // Отображаем результат
+    // 9. Получаем элемент для отображения результата
+    const resultElement = document.querySelector();
+
+    // 10. Отображаем результат
     if (isCorrectByIndex) {
-      resultElement.innerText = "Правильно!";
+     
     } else {
-      resultElement.innerText = "Неправильно!";
+      
     }
   });
 });
