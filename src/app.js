a34e1c09895a8e6a7e4f73f3681c35346b5ff127
Алексей Рыбак <rybak2108@yandex.ru>
2025-01-14 17:42:31 +0300

    feature initDelete

--- src/app.js
+++ src/app.js
@@ -3,26 +3,26 @@ import {
   initDeleteCompleted,
   initAddTodo,
   initChangeStatus,
+  initDelete,
   updateTask,
   initDownload,
   downloadButton,
+  renderData,
 } from "./components/index.js";
 
-import { getTodos, toggleTodoStatus, deleteTodo } from "./API/index.js";
+import { getTodos } from "./API/index.js";
 
 import { showError, showLoader, hideLoader } from "./utils/helpers.js";
 
 export const container = document.getElementById("posts-container");
-export const deleteCompletedButton = document.getElementById(
-  "delete-completed-button"
-);
+
 
 export async function loadData() {
   try {
     showLoader();
     const todos = await getTodos();
 
-    renderData(todos);
+    renderData(todos, container);
   } catch (error) {
     console.error(error.message);
     if (error.message === "Задач нет") {
@@ -35,84 +35,6 @@ export async function loadData() {
   }
 }
 
-function renderData(todos) {
-  container.innerHTML = "";
-
-  const hasCompletedTodos = todos.some((todo) => todo.completed);
-
-  deleteCompletedButton.style.display = hasCompletedTodos ? "block" : "none";
-
-  todos.forEach((todo) => {
-    const todoElement = document.createElement("div");
-    todoElement.classList.add("todo");
-    todoElement.setAttribute("data-id", todo.id);
-
-    const checkbox = document.createElement("input");
-    checkbox.type = "checkbox";
-    checkbox.checked = todo.completed;
-
-    initChangeStatus(todo, checkbox);
-
-    const textElement = document.createElement("p");
-    textElement.textContent = todo.text;
-    textElement.style.textDecoration = todo.completed ? "line-through" : "none";
-
-    const timeElement = document.createElement("p");
-    timeElement.textContent = new Date(todo.createdAt).toLocaleString("ru-RU", {
-      year: "numeric",
-      month: "numeric",
-      day: "numeric",
-      hour: "2-digit",
-      minute: "2-digit",
-    });
-
-    const deleteButton = document.createElement("button");
-    deleteButton.classList.add("button-function");
-
-    const deleteIcon = document.createElement("img");
-    deleteIcon.src = "./assets/icons/icon-delete.png";
-    deleteIcon.alt = "Удалить";
-    deleteIcon.title = "Удалить";
-
-    deleteButton.append(deleteIcon);
-
-    deleteButton.addEventListener("click", async () => {
-      try {
-        await deleteTodo(todo.id);
-        await loadData();
-      } catch (error) {
-        console.error(error.message);
-        showError("Не удалось удалить задачу");
-      }
-    });
-
-    const updateButton = document.createElement("button");
-    updateButton.classList.add("button-function");
-
-    const updateIcon = document.createElement("img");
-    updateIcon.src = "./assets/icons/icon-update.png";
-    updateIcon.alt = "Изменить";
-    updateIcon.title = "Изменить";
-
-    updateButton.append(updateIcon);
-
-    updateButton.addEventListener("click", () => updateTask(todo));
-
-    todoElement.append(
-      checkbox,
-      textElement,
-      timeElement,
-      deleteButton,
-      updateButton
-    );
-
-    initDragAndDrop(todoElement, todo, container);
-    container.append(todoElement);
-    downloadButton.hidden = true;
-    hideLoader();
-  });
-}
-
 initAddTodo();
-initDeleteCompleted();
+initDeleteCompleted(container);
 initDownload();
