ac85170d782e0da17b2a363facaff44d18f023cc
Алексей Рыбак <rybak2108@yandex.ru>
2025-01-14 15:54:10 +0300

    feature initAddTodo

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import {
   initDragAndDrop,
   initDeleteCompleted,
-  addNewTodo,
+  initAddTodo,
 } from "./components/index.js";
 
 import {
@@ -9,14 +9,11 @@ import {
   toggleTodoStatus,
   deleteTodo,
   updateTodo,
-  addTodo,
 } from "./API/index.js";
 
 import { showError, showLoader, hideLoader } from "./utils/helpers.js";
 
 export const container = document.getElementById("posts-container");
-export const taskInput = document.getElementById("task-input");
-const addButton = document.getElementById("add-button");
 const downloadButton = document.querySelector(".button-download");
 export const deleteCompletedButton = document.getElementById(
   "delete-completed-button"
@@ -150,14 +147,7 @@ function renderData(todos) {
   });
 }
 
-addButton.addEventListener("click", () => addNewTodo(taskInput));
-
-taskInput.addEventListener("keydown", (event) => {
-  if (event.key === "Enter") {
-    addNewTodo(taskInput);
-  }
-});
-
 downloadButton.addEventListener("click", loadData);
 
+initAddTodo();
 initDeleteCompleted();
--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
 export * from "./inits/initDragAndDrop.js";
-export * from './inits/initDeleteCompleted.js'
+export * from "./inits/initDeleteCompleted.js";
+export * from "./inits/initAddTodo.js";
 
 export * from "./ui/updateTasksOrder.js";
-export * from './ui/addNewTodo.js'
+export * from "./ui/addNewTodo.js";
