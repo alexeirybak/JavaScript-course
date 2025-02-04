eef4ca46973527caa935d9989445a309010c4f69
Алексей Рыбак <rybak2108@yandex.ru>
2025-02-04 13:23:28 +0300

    check3

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-const img = document.createElement('img');
-img.src = 'img/image.png';
-img.alt = 'Рисунок';
-
-document.body.append(img);
-
 const block = document.getElementById('block');
 const heading = document.createElement('h1');
 heading.innerText = 'Привет, разработчики!';
