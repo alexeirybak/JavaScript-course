a14ff5ac7dea267b5ae880c326bdbdeb6f17d0ee
Алексей Рыбак <rybak2108@yandex.ru>
2025-02-04 13:22:54 +0300

    check2

--- script.js
+++ script.js
@@ -7,22 +7,5 @@ document.body.append(img);
 const block = document.getElementById('block');
 const heading = document.createElement('h1');
 heading.innerText = 'Привет, разработчики!';
-heading.classList.add('heading');
+heading.classlist.add('heading');
 block.append(heading);
-
-let a = 5;
-let b = 5;
-
-if (a === b) {
-  let c = a + b;
-  console.log(c);
-}
-
-const initialArray = [];
-
-const getArray = () => {
-  const array = initialArray.push[1];
-  console.log(array);
-};
-
-getArray();
