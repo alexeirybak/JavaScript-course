dfb7566fb4d602c5aebccdad6992fb642b250dd2
Алексей Рыбак <rybak2108@yandex.ru>
2025-02-17 20:12:02 +0300

    check

--- script.js
+++ script.js
@@ -1,38 +1,59 @@
-const dog1 = {
-  name: "Шарик",
-  breed: "корги",
-  weight: 7,
-  bark() {
-    console.log(`${this.name} лает: Гав!`);
-  },
-  increaseWeight() {
-    console.log(this.weight + 1);
-  },
-  decreaseWeight() {
-    console.log(this.weight - 1);
-  },
-};
+class Dog {
+  constructor(name, breed, weight = 3) {
+    this.name = name;
+    this.breed = breed;
+    this.weight = weight;
+  }
 
-const dog2 = {
-  name: "Бобик",
-  breed: "шпиц",
-  weight: 5,
-  bark() {
-    console.log(`${this.name} лает: Тяв!`);
-  },
-  increaseWeight() {
-    console.log(this.weight + 1);
-  },
-  decreaseWeight() {
-    console.log(this.weight - 1);
-  },
-};
+  increaseWeight(amount = 1) {
+    this.weight += amount;
+    console.log(this.weight);
+  }
 
-dog1.bark();
-dog2.bark();
+  decreaseWeight(amount = 1) {
+    this.weight -= amount;
+    console.log(this.weight);
+  }
+}
 
-dog1.increaseWeight();
-dog2.increaseWeight();
+// const dog1 = new Dog("Шарик", "корги");
 
-dog1.decreaseWeight();
-dog2.decreaseWeight();
+// console.log(dog1);
+
+// dog1.increaseWeight();
+
+// console.log(dog1);
+
+class HuntingDog extends Dog {
+  constructor(name, breed, weight = 10, isTrained = false) {
+    super(name, breed, weight);
+    this.isTrained = isTrained;
+  }
+
+  duckHunt() {
+    this.increaseWeight(0.5);
+    this.isTrained = true;
+    console.log(`${this.name} поймал утку! Теперь он дрессированный!`);
+  }
+}
+
+const dog2 = new HuntingDog("Бим", "лайка", 20);
+
+class RetrievingDog extends HuntingDog {
+  constructor(name, breed, weight = 10, isTrained) {
+    super(name, breed, weight, isTrained);
+  }
+
+  retrieve(item) {
+    console.log(`${this.name} вернул ${item}!`);
+    this.increaseWeight(0.25);
+  }
+}
+
+const dog3 = new RetrievingDog("Рекс", "ретривер", 8);
+
+dog3.retrieve("мячик");
+
+dog3.duckHunt();
+
+console.log(dog3);
